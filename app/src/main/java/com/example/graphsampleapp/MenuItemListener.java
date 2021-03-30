e6a1f0f4e386fc09126ad6688c21c8fc9a87cf5e
unthinkable-lap-0070 <dhruv.goyal@unthinkable.co>
2021-03-30 13:24:14 +0530

    Menu Items Added

--- app/src/main/java/com/example/graphsampleapp/MenuItemListener.java
+++ app/src/main/java/com/example/graphsampleapp/MenuItemListener.java
@@ -3,8 +3,11 @@ package com.example.graphsampleapp;
 import android.view.MenuItem;
 
 public class MenuItemListener {
-    public static boolean itemListener(MenuItem item){
+    public static boolean itemListener(MenuItem item) {
 
+        if (item.getItemId() == R.id.steps) {
+           
+        }
         //add items here
         return false;
     }
